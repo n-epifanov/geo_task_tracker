7150633c5e0106f70122ad147a92c3a91a8a70ec
Nikolay Epifanov <nik.epifanov@gmail.com>
2021-04-05 16:50:21 +0300

    mix phx.gen.json --no-schema Tracker Task tasks

---
+++ lib/geo_task_tracker_web/views/task_view.ex
@@ -0,0 +1,16 @@
+defmodule GeoTaskTrackerWeb.TaskView do
+  use GeoTaskTrackerWeb, :view
+  alias GeoTaskTrackerWeb.TaskView
+
+  def render("index.json", %{tasks: tasks}) do
+    %{data: render_many(tasks, TaskView, "task.json")}
+  end
+
+  def render("show.json", %{task: task}) do
+    %{data: render_one(task, TaskView, "task.json")}
+  end
+
+  def render("task.json", %{task: task}) do
+    %{id: task.id}
+  end
+end
---
+++ test/geo_task_tracker/tracker_test.exs
@@ -0,0 +1,62 @@
+defmodule GeoTaskTracker.TrackerTest do
+  use GeoTaskTracker.DataCase
+
+  alias GeoTaskTracker.Tracker
+
+  describe "tasks" do
+    alias GeoTaskTracker.Tracker.Task
+
+    @valid_attrs %{}
+    @update_attrs %{}
+    @invalid_attrs %{}
+
+    def task_fixture(attrs \\ %{}) do
+      {:ok, task} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Tracker.create_task()
+
+      task
+    end
+
+    test "list_tasks/0 returns all tasks" do
+      task = task_fixture()
+      assert Tracker.list_tasks() == [task]
+    end
+
+    test "get_task!/1 returns the task with given id" do
+      task = task_fixture()
+      assert Tracker.get_task!(task.id) == task
+    end
+
+    test "create_task/1 with valid data creates a task" do
+      assert {:ok, %Task{} = task} = Tracker.create_task(@valid_attrs)
+    end
+
+    test "create_task/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Tracker.create_task(@invalid_attrs)
+    end
+
+    test "update_task/2 with valid data updates the task" do
+      task = task_fixture()
+      assert {:ok, %Task{} = task} = Tracker.update_task(task, @update_attrs)
+    end
+
+    test "update_task/2 with invalid data returns error changeset" do
+      task = task_fixture()
+      assert {:error, %Ecto.Changeset{}} = Tracker.update_task(task, @invalid_attrs)
+      assert task == Tracker.get_task!(task.id)
+    end
+
+    test "delete_task/1 deletes the task" do
+      task = task_fixture()
+      assert {:ok, %Task{}} = Tracker.delete_task(task)
+      assert_raise Ecto.NoResultsError, fn -> Tracker.get_task!(task.id) end
+    end
+
+    test "change_task/1 returns a task changeset" do
+      task = task_fixture()
+      assert %Ecto.Changeset{} = Tracker.change_task(task)
+    end
+  end
+end
