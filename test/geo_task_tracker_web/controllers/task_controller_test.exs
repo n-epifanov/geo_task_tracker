b388a9ebe83b7a9ae53b536d188b618891acb24b
Nikolay Epifanov <nik.epifanov@gmail.com>
2021-04-07 17:32:04 +0300

    Add some tests

--- test/geo_task_tracker_web/controllers/task_controller_test.exs
+++ test/geo_task_tracker_web/controllers/task_controller_test.exs
@@ -1,86 +1,72 @@
 defmodule GeoTaskTrackerWeb.TaskControllerTest do
   use GeoTaskTrackerWeb.ConnCase
 
-  alias GeoTaskTracker.Tracker
-  alias GeoTaskTracker.Tracker.Task
-
-  @create_attrs %{
-
-  }
-  @update_attrs %{
-
-  }
-  @invalid_attrs %{}
-
-  def fixture(:task) do
-    {:ok, task} = Tracker.create_task(@create_attrs)
-    task
-  end
+  alias GeoTaskTracker.Models.Task
+  alias GeoTaskTracker.Models.Token
+  alias GeoTaskTracker.Repo
 
   setup %{conn: conn} do
+    Task.create!({44, 45}, {55, 56})
+    Task.create!({44, 45.001}, {55, 56})
+    Task.create!({44, 46}, {55, 56})
+
+    Repo.insert!(%Task{
+      delivery: %Geo.Point{coordinates: {44, 45}},
+      pickup: %Geo.Point{coordinates: {55, 56}},
+      status: :done
+    })
+
+    Repo.insert!(%Token{token: "driver_token", role: "driver"})
+    Repo.insert!(%Token{token: "manager_token", role: "manager"})
+
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
   end
 
   describe "index" do
-    test "lists all tasks", %{conn: conn} do
-      conn = get(conn, Routes.task_path(conn, :index))
-      assert json_response(conn, 200)["data"] == []
+    test "lists nearest tasks", %{conn: conn} do
+      conn = put_req_header(conn, "authorization", "Bearer driver_token")
+      conn = get(conn, Routes.task_path(conn, :index), lon: 44, lat: 45)
+
+      assert %{
+               "status" => "ok",
+               "tasks" => [
+                 %{"id" => _, "lat" => 45.0, "lon" => 44.0},
+                 %{"id" => _, "lat" => 45.001, "lon" => 44.0}
+               ]
+             } = json_response(conn, 200)
     end
   end
 
+  @create_params [pickup: %{lat: 55, lon: 66}, delivery: %{lat: 77, lon: 88}]
+
   describe "create task" do
-    test "renders task when data is valid", %{conn: conn} do
-      conn = post(conn, Routes.task_path(conn, :create), task: @create_attrs)
-      assert %{"id" => id} = json_response(conn, 201)["data"]
+    test "creates task when data is valid", %{conn: conn} do
+      conn = put_req_header(conn, "authorization", "Bearer manager_token")
+      conn = post(conn, Routes.task_path(conn, :create), @create_params)
+      assert json_response(conn, 200) == %{"status" => "ok"}
+    end
 
-      conn = get(conn, Routes.task_path(conn, :show, id))
+    test "with driver token returns 403", %{conn: conn} do
+      conn = put_req_header(conn, "authorization", "Bearer driver_token")
+      conn = post(conn, Routes.task_path(conn, :create), @create_params)
+      assert json_response(conn, 403) == %{"status" => "error", "error_reason" => "forbidden"}
+    end
 
-      assert %{
-               "id" => id
-             } = json_response(conn, 200)["data"]
+    test "with no token returns 403", %{conn: conn} do
+      conn = post(conn, Routes.task_path(conn, :create), @create_params)
+      assert json_response(conn, 403) == %{"status" => "error", "error_reason" => "forbidden"}
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
-      conn = post(conn, Routes.task_path(conn, :create), task: @invalid_attrs)
-      assert json_response(conn, 422)["errors"] != %{}
+      conn = put_req_header(conn, "authorization", "Bearer manager_token")
+
+      conn =
+        post(conn, Routes.task_path(conn, :create),
+          pickup: %{lat: "A", lon: 66},
+          delivery: %{lat: 77, lon: 88}
+        )
+
+      assert json_response(conn, 200) == %{"error_reason" => %{"lat" => ["is invalid"]}, "status" => "error"}
     end
-  end
-
-  describe "update task" do
-    setup [:create_task]
-
-    test "renders task when data is valid", %{conn: conn, task: %Task{id: id} = task} do
-      conn = put(conn, Routes.task_path(conn, :update, task), task: @update_attrs)
-      assert %{"id" => ^id} = json_response(conn, 200)["data"]
-
-      conn = get(conn, Routes.task_path(conn, :show, id))
-
-      assert %{
-               "id" => id
-             } = json_response(conn, 200)["data"]
-    end
-
-    test "renders errors when data is invalid", %{conn: conn, task: task} do
-      conn = put(conn, Routes.task_path(conn, :update, task), task: @invalid_attrs)
-      assert json_response(conn, 422)["errors"] != %{}
-    end
-  end
-
-  describe "delete task" do
-    setup [:create_task]
-
-    test "deletes chosen task", %{conn: conn, task: task} do
-      conn = delete(conn, Routes.task_path(conn, :delete, task))
-      assert response(conn, 204)
-
-      assert_error_sent 404, fn ->
-        get(conn, Routes.task_path(conn, :show, task))
-      end
-    end
-  end
-
-  defp create_task(_) do
-    task = fixture(:task)
-    %{task: task}
   end
 end
