a0fbec3866cd23e2d61c5a825ae4b18045c53c17
Nikolay Epifanov <nik.epifanov@gmail.com>
2021-04-07 18:11:15 +0300

    Add Geo points to seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -11,9 +11,26 @@
 # and so on) as they will fail if something goes wrong.
 
 alias GeoTaskTracker.Repo
+alias GeoTaskTracker.Models.Task
 alias GeoTaskTracker.Models.Token
 
 Repo.insert!(%Token{token: "some_valid_driver_token", role: "driver"})
 Repo.insert!(%Token{token: "another_valid_driver_token", role: "driver"})
 Repo.insert!(%Token{token: "some_valid_manager_token", role: "manager"})
 Repo.insert!(%Token{token: "another_valid_manager_token", role: "manager"})
+
+Repo.insert!(%Task{
+  delivery: %Geo.Point{coordinates: {44, 45}},
+  pickup: %Geo.Point{coordinates: {55, 56}},
+  status: :new
+})
+Repo.insert!(%Task{
+  delivery: %Geo.Point{coordinates: {44.001, 45}},
+  pickup: %Geo.Point{coordinates: {55, 56}},
+  status: :new
+})
+Repo.insert!(%Task{
+  delivery: %Geo.Point{coordinates: {44, 45}},
+  pickup: %Geo.Point{coordinates: {55, 56}},
+  status: :done
+})
