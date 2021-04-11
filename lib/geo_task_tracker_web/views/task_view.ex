3c314529d07e13ab99b25365ad3fbcadc28641e2
Nikolay Epifanov <nik.epifanov@gmail.com>
2021-04-11 17:08:36 +0300

    Remove unused code

--- lib/geo_task_tracker_web/views/task_view.ex
+++ lib/geo_task_tracker_web/views/task_view.ex
@@ -10,10 +10,6 @@ defmodule GeoTaskTrackerWeb.TaskView do
     %{status: :ok, tasks: render_many(tasks, TaskView, "task.json")}
   end
 
-  def render("show.json", %{task: task}) do
-    %{data: render_one(task, TaskView, "task.json")}
-  end
-
   def render("task.json", %{task: task}) do
     %{id: task.id, lon: task.lon, lat: task.lat}
   end
